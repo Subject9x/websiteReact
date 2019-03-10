4e151ebe01ed5034da45317ecf64cc81f7e8ae91
subject9x <pwroohr@gmail.com>
2019-03-09 21:19:19 -0500

    Initial commit
    + standing up new website based on create-react-app in React

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+/*
+    Home page of whole application?
+    maybe
+*/
+class HomePage extends Component{
+    render(){
+        return(
+            <button className="foo">
+                {/*TODO*/}
+            </button>
+        );
+    }
+}
