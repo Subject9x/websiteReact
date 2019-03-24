1b00132cde98743a70fac84badc19d75e4b33384
subject9x <pwroohr@gmail.com>
2019-03-23 21:10:40 -0400

    shifted thumbnails
    + 2 rows looks nicer than 1 squished row
    + added new thumbnail images.

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -17,19 +17,25 @@ class HomePage extends Component{
                         <h1>Roohr Orbital Yards</h1>
                     </Col>
                 </Row>
+                <Row><Col><p></p></Col></Row>
                 <Row>
                     <Col md={{ size: 6, offset: 3 }}>
                     <BillBoard />
                     </Col>
                 </Row>
+                <Row><Col><p></p></Col></Row>
+                <Row><Col><p></p></Col></Row>
                 <Row>
-                    
+                    <Col md={{ size: 2 }}><Button><img alt="[Code Work]" src='/img/thumbs/codeThumb.png'></img></Button></Col>
+                    <Col md={{ size: 2, offset: 2 }}><Button><img alt="[Artwork]" src='/img/thumbs/artThumb.png'></img></Button></Col>
+                    <Col md={{ size: 2 , offset: 3}}><Button><img alt="[Board Games]" src='/img/thumbs/tableThumb.png'></img></Button></Col>
                 </Row>
                 <Row>
-                    <Col><Button>About</Button></Col>
-                    <Col><Button><img src='/img/thumbs/codeThumb.png'></img></Button></Col>
-                    <Col><Button><img src='/img/thumbs/artThumb.png'></img></Button></Col>
-                    <Col><Button>Board Games</Button></Col>
+                    <Col><p></p></Col>
+                </Row>
+                <Row>
+                    <Col md={{ size: 2, offset: 3 }}><Button>About</Button></Col>
+                    <Col md={{ size: 2, offset: 1}}><Button><img alt="[Other Sites]" src='/img/thumbs/netThumb.png'></img></Button></Col>
                 </Row>
             </Container>
         );
